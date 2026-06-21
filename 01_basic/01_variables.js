793dbdf39b140c31020fc7acb6ebacdf89b5ae5c
manan-me <abdulmanan30m@gmail.com>
2026-06-21 03:04:22 +0000

    Variables study

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,21 @@
+const accountId=1234
+let email="manan@gmail.com"
+var password=""
+accountCity="Lahore"
+let accountState;
+
+
+email="fiza@gmail.com"
+password="21212121"
+accountCity="fORTaBBAS"
+
+
+// prefer not to use var
+// because of block scope  and functional scope
+
+
+console.table([accountId,email,password,accountCity,accountState]);
+
+
+
+ 
