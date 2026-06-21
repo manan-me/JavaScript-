a6b056fe044b73b103ce72a823fadb1bdaabf030
manan-me <abdulmanan30m@gmail.com>
2026-06-21 09:00:36 +0000

    dataTypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all Js Code as newer version
+
+//alert(3+3)
+
+console.log(3+
+    3);                 // wrong , code readibility should be high
+
+
+console.log("manan");
+
+
+let name="manan"
+let age=21
+let isLoggedIn=true
+
+
+// Primitive data types
+// number= 2 to the power of 53 or 52 range
+// bigInt    
+// string => ""
+// boolean =>  true or false
+// null   =>  is a standalone value
+// undefined => empty but can take value
+// symbol =>mostly used during react, uniquenesss
+
+//objects
+console.log(typeof age) 
+
+console.log(typeof null);  // return object 
+
+console.log(typeof undefined);  // undefined
+
