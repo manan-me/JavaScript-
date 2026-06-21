b036a85bf1e110afb8f5e90f4b31c3b1e9cd4669
manan_me <abdulmanan30m@gmail.com>
2026-06-21 20:28:08 +0500

    Conversion of Data Types

---
+++ 01_basic/03_conversionsOperations.js
@@ -0,0 +1,41 @@
+// let score= "33abc"
+// let score="33"
+// let score= 33
+//let score= null
+// let score= undefined
+let score="manan"
+console.log(typeof score);
+
+console.log(typeof(score));
+
+
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber)
+
+
+
+// 33 => 33
+// 33abc => NaN
+// true => 1
+// Undefined => NaN
+// Null => 0
+
+
+// let isLoggedin=1             true
+let isLoggedin="" ;               //false
+// let isLoggedin="Manan"          true
+
+
+let booleanIsLoggedIn=Boolean(isLoggedin)
+console.log(booleanIsLoggedIn);
+
+
+
+let number=55
+
+let stringNum=String(number)
+console.log(stringNum);
+console.log(typeof stringNum);
