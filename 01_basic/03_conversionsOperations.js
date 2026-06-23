1922690f86ee334b6fb58bdede613a48ace5ffaa
manan_me <abdulmanan30m@gmail.com>
2026-06-23 07:51:14 +0500

    Conversoion Operations

--- 01_basic/03_conversionsOperations.js
+++ 01_basic/03_conversionsOperations.js
@@ -39,3 +39,50 @@ let number=55
 let stringNum=String(number)
 console.log(stringNum);
 console.log(typeof stringNum);
+
+
+
+
+// *************###Operations###***************
+
+let value=3
+let negValue=-value
+
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3); //2^3
+// console.log(2%3); 
+
+
+
+let str1="Abdul"
+let str2=' Manan'
+let str3=str1+str2
+console.log(str3);
+
+
+// console.log("1" + 2);  //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122       //Read ECMA Script  7 Anstract Operations -->7.1,..
+// console.log(1 + 2 + '2'); //32
+
+
+// console.log(+true);  //1
+// console.log(true+);  // Not Allowed
+// console.log(+"");  // 0
+
+
+let num1,num2,num3
+
+num1=num2=num3=2+2  //not prefferd assignment way
+
+
+let gameCounter=100;
+++ gameCounter
+gameCounter++
+// prefix and postfix
+
