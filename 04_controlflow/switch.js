231f4a4054d50fabe2c3b6c836ff1dfae2e8d097
manan_me <abdulmanan30m@gmail.com>
2026-06-27 10:09:43 +0500

    switch in controlflow

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,29 @@
+/////////////////// Syntax
+// switch(key){
+//     case value:
+//        break;
+
+//     default:
+//         break
+// }
+
+const month=2
+switch (month) {
+    case 1:
+        console.log("January");
+        
+        break;
+    case 2:
+        console.log("February");
+        
+        
+    case 3:
+        console.log("March");
+        break;
+    
+
+    default:
+        console.log("NO month");
+        
+        break;
+}
