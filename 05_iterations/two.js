7dc2a94cfa78003399c3c0d9fb5fb9c9ae5de8aa
manan_me <abdulmanan30m@gmail.com>
2026-06-28 19:37:07 +0500

    while and do while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+///while looop
+
+
+
+
+// let i=0
+// while (i<=10) {
+//     console.log(`Hey this is i = ${i}`);
+//     i+=2
+    
+// }
+
+// let arr=["BatMan","SuperMan","IronMan"]
+// let j=0
+// while (j<arr.length) {
+//     console.log(`Here is our hero ${arr[j]}`);
+//     j++
+// }
+
+
+// now same with do while
+
+
+let k=0
+let arr1=["BatMan","SuperMan","IronMan"]
+do{
+    console.log(`Here is our hero ${arr1[k]}`);
+    k++
+}while(k<arr1.length);
